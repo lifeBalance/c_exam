0dbcab939ce7189850619eb7c588621bc3267377
Javier <javi@grey.local>
2022-05-22 18:30:00 +0300

    add main.c ft_itoa_base

--- 04/ft_itoa_base/main.c
+++ 04/ft_itoa_base/main.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char	*ft_itoa_base(int value, int base);
 
-int	main()
+int	main(int argc, char **argv)
 {
-	int	n;
-	int	base;
-
-	n = 255, base = 10;
-	printf("%d = %s(base: %d)\n", n, ft_itoa_base(n, base), base);
-	n = -255, base = 10;
-	printf("%d = %s(base: %d)\n", n, ft_itoa_base(n, base), base);
-	n = 0, base = 10;
-	printf("%d = %s(base: %d)\n", n, ft_itoa_base(n, base), base);
-	n = 0, base = 16;
-	printf("%d = %s(base: %d)\n", n, ft_itoa_base(n, base), base);
-	n = -255, base = 16;
-	printf("%d = %s(base: %d)\n", n, ft_itoa_base(n, base), base);
-	n = 15, base = 16;
-	printf("%d = %s(base: %d)\n", n, ft_itoa_base(n, base), base);
-	n = -1001, base = 10;
-	printf("%d = %s(base: %d)\n", n, ft_itoa_base(n, base), base);
+	if (argc == 3)
+		printf("%s\n", ft_itoa_base(atoi(argv[1]), atoi(argv[2])));
 	return (0);
 }
