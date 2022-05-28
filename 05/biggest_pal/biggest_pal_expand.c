3905dda34890f7dedaddf13414c549cfc1009e21
Javier <javi@grey.local>
2022-05-28 12:02:36 +0300

    refactor biggest_pal_expand.c (shorter)

--- 05/biggest_pal/biggest_pal_expand.c
+++ 05/biggest_pal/biggest_pal_expand.c
@@ -1,51 +1,38 @@
 #include <unistd.h>
 
-#define START	0
-#define END		1
+#define	START	0
+#define	END		1
 
-int	ft_strlen(char *s)
+void	expand(char *s, int start, int end, int sol[2])
 {
-	int len;
-
-	len = 0;
-	if (s)
-		while (s[len])
-			len++;
-	return (len);
-}
-
-void	expand(char *s, int start, int end, int len, int solution[2])
-{
-	while (start >= 0 && end < len && s[start] == s[end])
+	while (start >= 0 && s[end] && s[start] == s[end])
 	{
-		if (end - start + 1 >= solution[END] - solution[START] + 1)
+		if (end - start + 1 >= sol[END] - sol[START] + 1)
 		{
-			solution[START] = start;
-			solution[END] = end;
+			sol[START] = start;
+			sol[END] = end;
 		}
 		start--;
 		end++;
 	}
 }
 
-void biggest_pal(char *s)
+void	biggest_pal(char *s)
 {
-	int i;
-	int len;
-	int	solution[2] = {0};
+	int	i;
+	int	sol[2] = {0};
 
-	len = ft_strlen(s);
 	i = 0;
-	while (i < len)
-	{ 
-		expand(s, i, i, len, solution);		//  odd palindrome lengths
-		expand(s, i, i + 1, len, solution);	// even palindrome lengths
+	while(s[i])
+	{
+		expand(s, i, i, sol);		//  odd palindrome lengths
+		expand(s, i, i + 1, sol);	// even palindrome lengths
 		i++;
 	}
-	write(1, s + solution[START], solution[END] - solution[START] + 1);
+	write(1, s + sol[START], sol[END] - sol[START] + 1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		biggest_pal(argv[1]);
